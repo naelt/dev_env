c4ba2452b4e2e3c72861e8ae1f23cbffacd24646
Nael Tasmim <naeltasmim@yahoo.de>
2016-04-13 12:57:51 +0000

    update

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -d -p 80 --name webapp -v /home/core/share/webapp:/var/www/html/website nael01/nginx nginx
+docker run -d -p 80 --name webapp -v /home/core/share/:/var/www/html/website nael01/nginx nginx
